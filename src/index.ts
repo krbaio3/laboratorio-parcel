a36083cbaa084e667de8e63c49d62b8772750662
KrBaIO3 <krbaio3@gmail.com>
2020-10-18 21:21:04 +0200

    feat(home): home
    SCSS and Typescript

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
 console.log('entra');
+
+const index = () => {
+  const text = document.getElementsByClassName('text');
+  const textSayHello = text[0].getElementsByClassName('text__say-hello');
+  textSayHello[0].innerHTML = 'Hello Word';
+};
+
+index();
