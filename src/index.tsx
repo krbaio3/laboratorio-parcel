aaf69be74001b7180cd1c622218f806b85a70e1d
KrBaIO3 <krbaio3@gmail.com>
2020-10-20 23:21:49 +0200

    chore(react): react config
    React config, imagen optimized, bundler visualiser

---
+++ src/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { HelloComponent } from './hello';
+
+ReactDOM.render(<HelloComponent />, document.getElementById('parcel-root'));
+
+if (module.hot) {
+  module.hot.accept();
+}
